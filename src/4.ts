b45feddc937f9e6923771d012a27351354b32286
OlegPanadiy <opanadiy@gmail.com>
2023-11-30 15:44:32 +0100

    HW 03

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
+class Key {
+	private signature: number = Math.floor(Math.random() * 100) + 1;;
+
+
+	getSignature(): number {
+		return this.signature;
+	}
+}
+
+class Person {
+	constructor(private key: Key) {}
+
+	getKey(): Key {
+		return this.key;
+	}
+}
+
+abstract class House {
+	protected door: boolean = false;
+	protected key: Key;
+	tenants: Person[] = [];
+
+	constructor(key: Key) {
+		this.key = key;
+	  }
+
+	abstract openDoor(key: Key): void;
+
+	comeIn(person: Person): void {
+		if (this.door) {
+			this.tenants.push(person);
+		}
+	}
+}
+
+class MyHouse extends House {
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true;
+		}
+	}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
+
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
